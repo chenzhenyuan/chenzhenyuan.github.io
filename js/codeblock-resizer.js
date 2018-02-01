4073e88e48b290bfc1049c622d9759e4889d7580
chenzhenyuan <jason@chenzhenyuan.com>
2018-02-01 17:19:20 +0800

    Site updated: 2018-02-01 17:19:20

--- js/codeblock-resizer.js
+++ js/codeblock-resizer.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 01 2018 17:18:05 GMT+0800 (CST)
+// build time:Thu Feb 01 2018 17:19:19 GMT+0800 (CST)
 +function(i){"use strict";var t=function(t){this.$codeBlocks=i(t)};t.prototype={run:function(){var t=this;t.resize();i(window).smartresize(function(){t.resize()})},resize:function(){var t=this;t.$codeBlocks.each(function(){var t=i(this).find(".gutter");var r=i(this).find(".code");var n=r.width()-r.innerWidth();var e=i(this).outerWidth()-t.outerWidth()+n;r.css("width",e);r.children("pre").css("width",e)})}};i(document).ready(function(){i.fn.hasHorizontalScrollBar=function(){return this.get(0).scrollWidth>this.innerWidth()};var r=new t("figure.highlight");r.run()})}(jQuery);
 //rebuild by neat 
