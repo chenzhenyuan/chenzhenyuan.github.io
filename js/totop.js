3bd297a2348afc6fadca222ee42551745745f724
chenzhenyuan <jason@chenzhenyuan.com>
2018-02-01 17:12:10 +0800

    Site updated: 2018-02-01 17:12:10

--- js/totop.js
+++ js/totop.js
@@ -1,12 +1,3 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
+// build time:Thu Feb 01 2018 17:12:10 GMT+0800 (CST)
+$(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
+//rebuild by neat 
