485903990223ebaf895406f31d8176290deedc34
chenzhenyuan <jason@chenzhenyuan.com>
2018-02-02 17:06:25 +0800

    Site updated: 2018-02-02 17:06:25

--- js/smartresize.js
+++ js/smartresize.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 02 2018 17:02:27 GMT+0800 (CST)
+// build time:Fri Feb 02 2018 17:06:23 GMT+0800 (CST)
 +function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
 //rebuild by neat 
