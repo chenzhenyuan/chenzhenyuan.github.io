6457952f9cbc22360ef1977c771cac6e0c0374e1
chenzhenyuan <jason@chenzhenyuan.com>
2018-02-02 17:09:27 +0800

    Site updated: 2018-02-02 17:09:27

--- js/smartresize.js
+++ js/smartresize.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 02 2018 17:06:23 GMT+0800 (CST)
-+function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
+// build time:Fri Feb 02 2018 17:09:26 GMT+0800 (CST)
++function($,sr){var debounce=function(func,threshold,execAsap){var timeout;return function debounced(){var obj=this,args=arguments;function delayed(){if(!execAsap){func.apply(obj,args)}timeout=null}if(timeout){clearTimeout(timeout)}else if(execAsap){func.apply(obj,args)}timeout=setTimeout(delayed,threshold||100)}};jQuery.fn[sr]=function(fn){return fn?this.bind("resize",debounce(fn)):this.trigger(sr)}}(jQuery,"smartresize");
 //rebuild by neat 
