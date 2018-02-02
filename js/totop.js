ef0501614bf641c66fc7ff4962089dbfc5782cf5
chenzhenyuan <jason@chenzhenyuan.com>
2018-02-02 16:52:18 +0800

    Site updated: 2018-02-02 16:52:18

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 02 2018 16:47:33 GMT+0800 (CST)
+// build time:Fri Feb 02 2018 16:52:17 GMT+0800 (CST)
 $(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
