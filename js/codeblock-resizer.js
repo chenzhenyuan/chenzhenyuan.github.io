a74a5f616d49a5d5d36036446639ac9a4301a710
chenzhenyuan <jason@chenzhenyuan.com>
2018-02-05 11:02:10 +0800

    Site updated: 2018-02-05 11:02:10

--- js/codeblock-resizer.js
+++ js/codeblock-resizer.js
@@ -1,3 +1,3 @@
-// build time:Mon Feb 05 2018 10:18:00 GMT+0800 (CST)
+// build time:Mon Feb 05 2018 11:02:10 GMT+0800 (CST)
 +function(i){"use strict";var t=function(t){this.$codeBlocks=i(t)};t.prototype={run:function(){var t=this;t.resize();i(window).smartresize(function(){t.resize()})},resize:function(){var t=this;t.$codeBlocks.each(function(){var t=i(this).find(".gutter");var r=i(this).find(".code");var n=r.width()-r.innerWidth();var e=i(this).outerWidth()-t.outerWidth()+n;r.css("width",e);r.children("pre").css("width",e)})}};i(document).ready(function(){i.fn.hasHorizontalScrollBar=function(){return this.get(0).scrollWidth>this.innerWidth()};var r=new t("figure.highlight");r.run()})}(jQuery);
 //rebuild by neat 
--- js/smartresize.js
+++ js/smartresize.js
@@ -1,3 +1,3 @@
-// build time:Mon Feb 05 2018 10:18:00 GMT+0800 (CST)
+// build time:Mon Feb 05 2018 11:02:10 GMT+0800 (CST)
 +function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
 //rebuild by neat 
